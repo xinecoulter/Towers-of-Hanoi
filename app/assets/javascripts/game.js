f299c4264ff4f6f8f83af4c1a8aa40b10779455e
Christine Coulter <christinetcoulter@gmail.com>
2013-08-28 16:29:41 -0400

    Able to determine if piece is free to move.

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -7,18 +7,32 @@ var Game = {
         $('#play-area').on('click', '.piece', Game.choosePiece);
     },
     choosePiece: function(event) {
-        var piece;
-        var currentTower;
+        var piece,
+            towerValue,
+            currentTower;
         console.log("you clicked on a game piece");
-        console.log(event);
-        console.log(event.currentTarget.innerHTML);
-        console.log(typeof(event.currentTarget.innerHTML));
-        console.log(event.currentTarget.attributes["data-id"].value);
 
         piece = parseInt(event.currentTarget.innerHTML);
-        currentTower = parseInt(event.currentTarget.attributes["data-id"].value);
-        if (piece === ) {
+        towerValue = event.currentTarget.attributes["data-id"].value;
+        console.log(towerValue);
+        if (towerValue === "tower1") {
+            currentTower = Game.tower1;
+            console.log("currentTower: " + Game.tower1);
+        } else if (towerValue === "tower2") {
+            currentTower = Game.tower2;
+            console.log("currentTower: " + Game.tower2);
+        } else if (towerValue === "tower3") {
+            currentTower = Game.tower3;
+            console.log("currentTower: " + Game.tower3);
+        }
 
+        console.log("minimum value of current tower " + Array.min(currentTower));
+        console.log("piece " + piece);
+
+        if (piece === Array.min(currentTower)) {
+            console.log("yuuuup");
+        } else {
+            console.log("noooope");
         }
     }
 };
@@ -27,4 +41,8 @@ var Game = {
 $(document).ready(function() {
     console.log("your mom");
     $('body').on('click', '#play-button', Game.start);
+
+    Array.min = function( array ){
+        return Math.min.apply( Math, array );
+    };
 });

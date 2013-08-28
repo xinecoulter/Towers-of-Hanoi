176da794b150ca18b7efe1f295e0a5df96f96bee
Christine Coulter <christinetcoulter@gmail.com>
2013-08-28 15:56:50 -0400

    Try to compare tower array to data-id of piece.

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,16 +1,25 @@
 var Game = {
+    tower1: [1, 2, 3],
+    tower2: [],
+    tower3: [],
     start: function() {
         console.log("UHHHHHHM");
-        $('#play-area').on('click', '.piece', Game.move);
+        $('#play-area').on('click', '.piece', Game.choosePiece);
     },
-    move: function(event) {
-        var tower1 = [1, 2, 3],
-            tower2 = [],
-            tower3 = [];
+    choosePiece: function(event) {
+        var piece;
+        var currentTower;
         console.log("you clicked on a game piece");
         console.log(event);
-        console.log(event.currentTarget.html());
-        // if (event.currentTarget.innerHTML())
+        console.log(event.currentTarget.innerHTML);
+        console.log(typeof(event.currentTarget.innerHTML));
+        console.log(event.currentTarget.attributes["data-id"].value);
+
+        piece = parseInt(event.currentTarget.innerHTML);
+        currentTower = parseInt(event.currentTarget.attributes["data-id"].value);
+        if (piece === ) {
+
+        }
     }
 };
 

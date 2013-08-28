144033f34a6771023e6be5072293514630d6e197
Christine Coulter <christinetcoulter@gmail.com>
2013-08-28 13:40:28 -0400

    New Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TowersOfHanoi::Application.config.secret_key_base = 'd564e100acacaf2094a99d230f85a312f98c296de2101dcb913575f39d3d0e24468d87bc1eb0fd03c65f5d8c4ba6e9b86a95bf062f71d47e22ff331dc364b390'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TowersOfHanoi::Application.config.session_store :cookie_store, key: '_Towers_of_Hanoi_session'
